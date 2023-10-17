f63eea229d3e3257bec194d50570588503316a29
Rian Dave Brignas <rbrignas23@gmail.com>
2023-10-17 17:11:44 +0800

    Main-tsx-Validation

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -162,7 +162,7 @@ function Main() {
                         </>
                     ) : null}
                     <ul className="dropdown-menu dropdown-menu-dark" style={{ width:'300px'}}>
-                        <Link className="customer-nav-droplink" to='user_profile'>
+                        <Link className="customer-nav-droplink" to={`user_profile/${userId}`}>
                             <li><a className="dropdown-item">
                             <img className="dropdown-icon"  src={ profIcon } />VIEW PROFILE</a></li></Link>
 
